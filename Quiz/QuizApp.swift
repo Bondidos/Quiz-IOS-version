3c21218a8b78909bf37bcb3e28201083a8f74671
vladimir <vladimir.hodaryonok@idfeurasia.pro>
2023-10-07 21:19:04 +0300

    Initial Commit

---
+++ Quiz/QuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizApp.swift
+//  Quiz
+//
+//  Created by Vladimir on 7.10.23.
+//
+
+import SwiftUI
+
+@main
+struct QuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
